2f4c822d4a3bf10da423aa67b55c0e5855573a97
Masaya Fukazawa <fukazawa@infordio.co.jp>
2017-04-03 21:32:16 +0900

    あとちょっと！！

---
+++ workplace/workplace.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('a[href^="#"]').on('click', function(){
+        var speed = 400;
+        var href= $(this).attr('href');
+        var target = $(href == '#' || href == '' ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+    });
+});
+
+$(function() {
+    $("#culture-button").on('click', function() {
+        var flag = false;
+
+    })
+})
