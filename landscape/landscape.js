3a2cc41a2c7226068270ad3398980e2f90caddb3
Taichi Ito <ito@infordio.co.jp>
2017-04-24 15:07:48 +0900

    ・トップ歯車レスポンシブ対応 ・社内風景修正

--- landscape/landscape.js
+++ landscape/landscape.js
@@ -9,10 +9,22 @@ $(function() {
         maxHeight: '90%',
         opacity: 0.7
     });
+    for (var i = 1 ; i <= $('.inline').length ; i++ ) {
+        $('#inline_' + i + ' > div').html($('.inline_' + i + ' img').clone());
+        $('#inline_' + i + ' > p'  ).html($('.inline_' + i + ' span').text());
+    }
     $('.inline').colorbox({
         inline: true,
+        rel: 'photo',
+        transition: 'elastic',
+        closeButton: true,
+        slideshow: false,
+        slideshowAuto: false,
         maxWidth: '90%',
         maxHeight: '90%',
         opacity: 0.7
-    })
+    });
+    $('.inline-html').on('click', function() {
+        $.colorbox.next();
+    });
 });
