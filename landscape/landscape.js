23e88d818cb4796df873598fb6965f6fe3202576
Taichi Ito <ito@infordio.co.jp>
2017-04-25 02:55:44 +0900

    社内風景本番画像パス設定

--- landscape/landscape.js
+++ landscape/landscape.js
@@ -1,14 +1,4 @@
 $(function() {
-    $('.slide').colorbox({
-        rel: 'photo',
-        transition: 'elastic',
-        closeButton: true,
-        slideshow: false,
-        slideshowAuto: false,
-        maxWidth: '90%',
-        maxHeight: '90%',
-        opacity: 0.7
-    });
     for (var i = 1 ; i <= $('.inline').length ; i++ ) {
         $('#inline_' + i + ' > div').html($('.inline_' + i + ' img').clone());
         $('#inline_' + i + ' > p'  ).html($('.inline_' + i + ' span').text());
@@ -20,8 +10,8 @@ $(function() {
         closeButton: true,
         slideshow: false,
         slideshowAuto: false,
-        maxWidth: '90%',
         maxHeight: '90%',
+        height: '90%',
         opacity: 0.7
     });
     $('.inline-html').on('click', function() {
