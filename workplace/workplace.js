5490c787398fff42109463f5e7bd2465d19e2e0d
Masaya Fukazawa <fukazawa@infordio.co.jp>
2017-04-27 09:21:03 +0900

    box押下時に説明欄がオープン

--- workplace/workplace.js
+++ workplace/workplace.js
@@ -33,3 +33,27 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $("#cultureBox").on('click', function() {
+        $("#culture-checkbox").prop('checked', true);
+        $("#culture-detail-section").show("slow");
+        $("#culture-yajirusi").text("expand_less");
+    });
+});
+
+$(function() {
+    $("#welfareBox").on('click', function() {
+        $("#welfare-checkbox").prop('checked', true);
+        $("#welfare-detail-section").show("slow");
+        $("#welfare-yajirusi").text("expand_less");
+    });
+});
+
+$(function() {
+    $("#followupBox").on('click', function() {
+        $("#followup-checkbox").prop('checked', true);
+        $("#followup-detail-section").show("slow");
+        $("#followup-yajirusi").text("expand_less");
+    });
+});
